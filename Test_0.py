b02bb269ff93f5fb43cc3f55b2f3fcc1138a6252
Lumos <tsykalanovdima@gmail.com>
2023-08-29 21:42:27 +0100

    Обновления: Описание ваших изменений

--- Test_0.py
+++ Test_0.py
@@ -1,16 +1,26 @@
+import selenium
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+options = webdriver.ChromeOptions()
 
+print(selenium.__version__)
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    print('start')
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--no-gpu')
+    service = ChromeService(executable_path="/usr/bin/chromedriver")
+    driver = webdriver.Chrome(options=options, service=service)
     yield driver
     driver.quit()
-
+    print("passed")
 
 def test_login(driver):
     driver.get("https://www.saucedemo.com/")
@@ -25,7 +35,7 @@ def test_login(driver):
     Psw.send_keys('secret_sauce')
 
     assert "Swag Labs" in driver.title
-    driver.quit()
+    print("+")
 
 def button_and_check(driver):
     button = WebDriverWait(driver, 10).until(
@@ -35,6 +45,6 @@ def button_and_check(driver):
     try:
         test_text = driver.find_element(By.CLASS_NAME, 'app_logo').text
         assert test_text == 'Swag Labs', "False - "
-        print(" + ")
+        print("+")
     except Exception as e:
         print(" - ", e)
